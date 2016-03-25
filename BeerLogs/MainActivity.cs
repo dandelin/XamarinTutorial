354ed7f64c9174a8346b5f6d449d7587da8c2d7d
Wonjae Kim <fkdlvm1109@hotmail.com>
2016-03-25 16:26:23 +0900

    init

---
+++ BeerLogs/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace BeerLogs
+{
+	[Activity(Label = "BeerLogs", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		private List<string> mItems;
+		private ListView mListView;
+
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+			mListView = FindViewById<ListView>(Resource.Id.myListView);
+			
+			mItems = new List<string>();
+			foreach (var c in new string[]{ "bob", "tom", "jim" })
+			{
+				mItems.Add(c);
+			}
+
+			MyListViewAdapter adapter = new MyListViewAdapter(this, mItems);
+
+			mListView.Adapter = adapter;
+		}
+	}
+}
+
